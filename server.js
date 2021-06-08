8b3533567d120929f4dcbe42d647e72c0a83dc68
RogerBariles <rogerbariles@gmail.com>
2021-06-08 17:11:34 -0300

    correccion server.js

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
-var sslRedirect = require('heroku-ssl-redirect');
 
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// enable ssl redirect
-app.use(sslRedirect());
-
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/paseshow-mp'));
 
 app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname,'/dist/paseshow-mp/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/paseshow-mp/index.html'));
 });
 
 // Start t< he app by listening on the default Heroku port
