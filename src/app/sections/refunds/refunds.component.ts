60f39afc2cc33bcc3e147ba9a5c5451e7281d043
RogerBariles <rogerbariles@gmail.com>
2021-06-14 11:49:26 -0300

    se corrigio peticion en busca de reservas en devoluciones

--- src/app/sections/refunds/refunds.component.ts
+++ src/app/sections/refunds/refunds.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, OnChanges, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Reserva } from 'src/app/models/reservas.model';
 import { ReservasService } from 'src/app/services/reservas.service';
@@ -8,7 +8,7 @@ import { ReservasService } from 'src/app/services/reservas.service';
   templateUrl: './refunds.component.html',
   styleUrls: ['./refunds.component.scss']
 })
-export class RefundsComponent implements OnInit {
+export class RefundsComponent implements OnInit, AfterViewChecked {
 
   formSearch: FormGroup;
 
@@ -23,11 +23,20 @@ export class RefundsComponent implements OnInit {
     this.initFormSearch();
   };
 
+  ngAfterViewChecked(): void {
+    let listaRegistros = document.querySelectorAll('.listRegistro');
+    if (listaRegistros) {
+      listaRegistros.forEach(unRegistro => {
+        unRegistro.classList.add('registerAprovedd');
+      });
+    }
+  };
+
   initFormSearch(): void {
     this.formSearch = this.fb.group({
       id: [''],
       clienteDni: [''],
-      estado: ['E']
+      estado: ["'E'"]
     });
   };
 
